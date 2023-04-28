994a18add34f332c53629bd2b33e698a496ebd54
prakarsh.kaushik <prakarsh.kaushik@del1-lhp-n72318.synapse.com>
2023-04-28 11:59:51 +0530

    Initial Commit

---
+++ src/main/java/com/capstone/donorhub/DonorhubApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.donorhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonorhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonorhubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capstone/donorhub/DonorhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.donorhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonorhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
